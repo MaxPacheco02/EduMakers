67204ce66d9777a7cb66a5c74a9e33637e97c19b
MaxPacheco02 <a01552369@tec.mx>
2023-07-12 02:07:36 +0100

    Changes July 11th, started the code for the Krebs Cycle board

---
+++ EduMakers_CicloKrebs/main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import time
+import os
+from RPi import GPIO
+import subprocess
+ 
+os.system('clear') #clear screen, this is just for the OCD purposes
+ 
+step = 1 #linear steps for increasing/decreasing volume
+paused = False #paused state
+num = 0
+ 
+#tell to GPIO library to use logical PIN names/numbers, instead of the physical PIN numbers
+GPIO.setmode(GPIO.BCM) 
+ 
+#set up the pins we have been using
+clk = 14
+dt = 15
+sw = 18
+mag_1 = [21,20,16,12]
+mag_2 = [7,8,25,24]
+mag_in_1 = 19
+mag_in_2 = 26
+state = [0]*32
+last = [0]*32
+last_changed = -1
+ 
+#set up the GPIO events on those pins
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.setup(mag_in_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(mag_in_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+for i in range(4):
+        GPIO.setup(mag_1[i], GPIO.OUT)
+        GPIO.setup(mag_2[i], GPIO.OUT)
+ 
+#get the initial states
+counter = 0
+clkLastState = GPIO.input(clk)
+dtLastState = GPIO.input(dt)
+swLastState = GPIO.input(sw)
+ 
+#define functions which will be triggered on pin state changes
+def encoderClicked(channel):
+        global counter, step
+        clkState = not GPIO.input(clk)
+        dtState = not GPIO.input(dt)
+ 
+        if not clkState and dtState:
+                counter = counter + step
+        elif clkState and not dtState:
+                counter = counter - step
+        print ("Counter ", counter)
+
+def swClicked(channel):
+        global paused
+        paused = not paused
+        print ("Paused ", paused)
+
+def byte(num):
+       out = []
+       for i in range(3,-1,-1):
+              out.append((num >> i) & 1)
+       return out
+
+def mag_handler(index, val):
+        global state, last, last_changed
+        
+        if val:
+                rc = subprocess.check_call("./play.sh 'audio/%s.mp3'" % 1, shell=True)
+        
+
+#set up the interrupts
+GPIO.add_event_detect(clk, GPIO.RISING, callback=encoderClicked, bouncetime=50)
+GPIO.add_event_detect(dt, GPIO.RISING, callback=encoderClicked, bouncetime=50)
+GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=200)
+ 
+while True:
+        print("Paused:", int(paused), "Encoder:", counter, )
+        num = (num+1) % 32
+        sel = byte(num)
+        for i in range(4):
+                GPIO.output(mag_1[i], sel[i])
+                GPIO.output(mag_2[i], sel[i])
+        print(GPIO.input(mag_in_1))
+        mag_handler(num, GPIO.input(mag_in_1))
+        mag_handler(num+16, GPIO.input(mag_in_2))
+        time.sleep(1)
+
+GPIO.cleanup()
+
+'''
+Lines 41, 42, 52, 53 have negations needed to simulate the encoder with buttons.
+They need to be reversed again to work correctly in the final implementation.
+
+
+'''
